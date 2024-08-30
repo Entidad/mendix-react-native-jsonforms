accd5fd49b3e71b5037767989564f2eef7bd9046
Ockert van Schalkwyk <ockert.vanschalkwyk@entidad.io>
2024-08-30 15:53:29 +0200

    CheckBoxControl styling
    Signed-off-by: Ockert van Schalkwyk <ockert.vanschalkwyk@entidad.io>

--- src/renderer/controls/CheckBoxControl.tsx
+++ src/renderer/controls/CheckBoxControl.tsx
@@ -44,16 +44,24 @@ export function CheckBoxControl(props:any){
 	};
 	const renderCheckBox=()=>{
 		if(checked){
+				//<View style={styleTmp.iconCheck}>
 			return(
-				<View style={styleTmp.iconCheck}>
+				<View style={styles?.checkboxInput||{
+					width:styles?.iconCheck?.width||40,
+					height:styles?.iconCheck?.height||40
+				}}>
 					<Svg width="24" height="24" viewBox="0 0 24 24">
 						<Path fill={styles?.checkboxInput?.color||"#1A6251"} d="M19 0h-14c-2.762 0-5 2.239-5 5v14c0 2.761 2.238 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-8.959 17l-4.5-4.319 1.395-1.435 3.08 2.937 7.021-7.183 1.422 1.409-8.418 8.591z"/>
 					</Svg>
 				</View>
 			)
 		}else{
+				//<View style={styleTmp.iconUncheck}>
 			return(
-				<View style={styleTmp.iconUncheck}>
+				<View style={styles?.iconUncheck||{
+					width:styles?.iconUncheck?.width||40,
+					height:styles?.iconUncheck?.height||40
+				}}>
 					<Svg width="24" height="24" viewBox="0 0 24 24">
 						<Path fill={styles?.checkboxInput?.color||"#D4D4D4"} d="M5 2c-1.654 0-3 1.346-3 3v14c0 1.654 1.346 3 3 3h14c1.654 0 3-1.346 3-3v-14c0-1.654-1.346-3-3-3h-14zm19 3v14c0 2.761-2.238 5-5 5h-14c-2.762 0-5-2.239-5-5v-14c0-2.761 2.238-5 5-5h14c2.762 0 5 2.239 5 5z"/>
 					</Svg>
